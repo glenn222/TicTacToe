acb24007a05c931198d80f26df688705c082bab1
glenn222 <g2ngo@uwaterloo.ca>
2016-01-04 17:52:55 -0500

    Converted TicTacToe from a console application to a WPF application.

---
+++ TicTacToe/GameWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Interaction logic for GameWindow.xaml
+    /// </summary>
+    public partial class GameWindow : Window
+    {
+        public GameWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -8,6 +8,7 @@ namespace TicTacToe
 {
     class Program
     {
+        [STAThread] //STA-Single Threaded Apartment, refers to the threading model used by the current main thread.
         static void Main(string[] args)
         {
             string[,] TicTacToeBoard = new string[3, 3];
@@ -17,32 +18,36 @@ namespace TicTacToe
 
             GameEngine gameEngine = new GameEngine();
             GameVisualizer gameVisualizer = new GameVisualizer();
-            
-            while (true)
-            {
-                Console.WriteLine("Choose playing mode - 2 Players or Computer(AI)?");
-                var mode = Console.ReadLine().ToUpper();
-
-                // Polymorphism - Two objects are of the same type, but they can be interchangable and defined by the user
-                if (mode.Equals("2 PLAYERS"))
-                {
-                    player1 = new RealPlayer();
-                    player2 = new RealPlayer();
-                    break;
-                }
-                else if (mode.Equals("AI"))
-                {
-                    player1 = new RealPlayer();
-                    player2 = new AIPlayer();
-                    break;
-                }
-            }
-
             // Inject dependencies
             GameManager gameManager = new GameManager(gameVisualizer, gameEngine);
 
+            GameWindow gameWindow = new GameWindow();
+            gameWindow.ShowDialog();
+
+            //while (true)
+            //{
+            //    Console.WriteLine("Choose playing mode - 2 Players or Computer(AI)?");
+            //    var mode = Console.ReadLine().ToUpper();
+
+            //    // Polymorphism - Two objects are of the same type, but they can be interchangable and defined by the user
+            //    if (mode.Equals("2 PLAYERS"))
+            //    {
+            //        player1 = new RealPlayer();
+            //        player2 = new RealPlayer();
+            //        break;
+            //    }
+            //    else if (mode.Equals("AI"))
+            //    {
+            //        player1 = new RealPlayer();
+            //        player2 = new AIPlayer();
+            //        break;
+            //    }
+            //}
+
+
+
             // Play game just plays game, not about who the players are, so its better to pass in the players to play the game.
-            gameManager.PlayGame(TicTacToeBoard, player1, player2);
+            //gameManager.PlayGame(TicTacToeBoard, player1, player2);
 
 
         }
