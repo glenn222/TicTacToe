a68c811bacea0fd698f3f6069512badb68ad99e8
glenn222 <glendon.ngo@gmail.com>
2019-07-03 00:22:11 -0400

    Initial Commit of Tic Tac Toe Project

---
+++ TicTacToe/RealPlayer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class RealPlayer : IPlayer
+    {
+        private string player;
+        private string position;
+
+        public string GetPlayer()
+        {
+            return player;
+        }
+
+        public void Play(string[,] TicTacToeBoard)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Enter position X for {0}", player);
+                    int positionX = Int32.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter position Y for {0}", player);
+                    int positionY = Int32.Parse(Console.ReadLine());
+
+                    position = positionX + "," + positionY;
+
+                    if (TicTacToeBoard[positionX, positionY].Equals(" "))
+                    {
+                        Console.WriteLine("You placed an {0} at position ({1}):", player, position);
+                        TicTacToeBoard[positionX, positionY] = player;
+                        break;
+                    }
+                    else
+                        Console.WriteLine("Position ({0},{1}) is already taken. Choose another position", positionX, positionY);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("This is not a valid position. Choose another position");
+                }
+            }
+        }
+
+        public void SetPlayer(string player)
+        {
+            this.player = player;
+        }
+
+    }
+}
